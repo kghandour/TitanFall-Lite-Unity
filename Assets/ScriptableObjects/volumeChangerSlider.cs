f87088b3bfd1722df6e4d6eb44900830f25158df
MarwanYoussef <Marwanusif1@hotmail.com>
2019-12-04 15:05:36 +0200

    merge

---
+++ Assets/ScriptableObjects/volumeChangerSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class volumeChangerSlider : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    private float musicVolume = 1.0f;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void setVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
