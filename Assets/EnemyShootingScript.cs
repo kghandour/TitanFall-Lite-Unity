1cc577c536bb3d6abf13588a9f3b4219246ea6ec
omarelmoghazy <omarelmoghazy1@gmail.com>
2019-12-06 16:25:37 +0200

    in progress

---
+++ Assets/EnemyShootingScript.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootingScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public GameObject shotStart;
+
+
+    private Animator anim;
+    private Transform player;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //Debug.Log("7aa7aa");
+    }
+
+
+    void Update()
+    {
+        if (anim.GetBool("isFiring"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        //Debug.DrawRay(shotStart.transform.position, shotStart.transform.forward, Color.red, 30.0f, true);
+        //Debug.Log("7aa7aa");
+        if (Physics.Raycast(shotStart.transform.position, shotStart.transform.forward, out hit, range))
+        {
+            if (hit.transform.name == "FPSController")
+                Debug.Log(hit.transform.name);
+        }
+    }
+
+
+    //public float maxDamage = 120f;
+    //public float minDamage = 45f;
+    //public float flashIntensity = 3f;
+    //public float fadeSpeed = 10f;
+
+    //private Animator anim;
+    //private LineRenderer laserShotLine;
+    //private Light laserShotLight;
+    //private SphereCollider col;
+    //private Transform player;
+    //private bool shooting;
+    //private float scaledDamage;
+
+
+    //private void Start()
+    //{
+    //    anim = GetComponent<Animator>();
+    //    laserShotLine = GetComponentInChildren<LineRenderer>();
+    //    laserShotLight = laserShotLine.gameObject.GetComponent<Light>();
+    //    col = GetComponent<SphereCollider>();
+    //    player = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+    //    laserShotLine.enabled = false;
+    //    laserShotLight.intensity = 0;
+
+    //    scaledDamage = maxDamage - minDamage;
+    //}
+
+    //private void Update()
+    //{
+    //    if (anim.GetBool("isFiring"))
+    //    {
+    //        Shoot(); 
+    //    }
+    //    else
+    //    {
+    //        laserShotLine.enabled = false;
+    //    }
+
+    //    laserShotLight.intensity = Mathf.Lerp(laserShotLight.intensity, 0f, fadeSpeed * Time.deltaTime);
+    //}
+
+    //private void OnAnimatorIK(int layerIndex)
+    //{
+    //}
+
+    //void Shoot()
+    //{
+    //    float fractionalDistance = (col.radius - Vector3.Distance(transform.position, player.position)) / col.radius;
+    //    float damage = scaledDamage * fractionalDistance + minDamage;
+    //}
+
+    //void ShotEffects()
+    //{
+    //    laserShotLine.SetPosition(0, laserShotLine.transform.position);
+    //    laserShotLine.SetPosition(1, player.position + Vector3.up * 1.5f);
+    //    laserShotLine.enabled = false;
+    //    laserShotLight.intensity = flashIntensity;
+    //}
+
+
+}
---
+++ Assets/EnemyTitanScript.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyTitanScript : MonoBehaviour
+{
+
+    protected NavMeshAgent enemy;
+    protected StateEnum State;
+    protected TargetScript[] potentialTargets;
+    protected TargetScript target;
+    protected TargetScript currentTarget;
+
+    private float nextActionTime = 0.0f;
+    private float period = 5f;
+    private float firingPeriod = 3f;
+
+
+    public Animator anim;
+    private Animation anima;
+
+    public GameObject player;
+    public float allowedRange;
+    public bool attackTrigger;
+
+    public GameObject gun;
+
+    private bool isFollowingFlag = true;
+    private bool isWalkingFlag = true;
+
+
+
+
+    //private IEnumerator WaitShwia(float shwia)
+    //{
+    //    //do
+    //    //{
+    //    //    yield return null;
+    //    //} while (animation.isPlaying);
+
+    //    yield return new WaitForSeconds(shwia);
+    //}
+
+    void FiringRoutine()
+    {
+        anim.SetBool("isFollowing", false);
+        anim.SetBool("isFiring", true);
+        //anim.SetBool("isFollowing", true);
+        //anim.SetBool("isFiring", false);
+        //anima.Play();
+        ////WaitShwia(2.06f);
+        //anim.SetBool("isFollowing", true);
+        //anim.SetBool("isFiring", false);
+        //anim.SetBool("isFiring", !anim.GetBool("isFiring"));
+    }
+
+    void WalkingRoutine()
+    {
+        anim.SetBool("isWalking", !anim.GetBool("isWalking"));
+        anim.SetBool("isIdle", !anim.GetBool("isIdle"));
+    }
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        anima = GetComponent<Animation>();
+        potentialTargets = FindObjectsOfType<TargetScript>();
+        target = potentialTargets[Random.Range(0, potentialTargets.Length)];
+        InvokeRepeating("WalkingRoutine", 0, 5f);
+        State = StateEnum.RUN;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < allowedRange && !anim.GetBool("isFollowing") && isFollowingFlag)
+        {
+            anim.SetBool("isFollowing", true);
+            isWalkingFlag = false;
+            isFollowingFlag = false;
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", false);
+            CancelInvoke();
+            InvokeRepeating("FiringRoutine", 3f, 3f);
+        }
+
+        if (anim.GetBool("isFollowing") && !anim.GetBool("isFiring"))
+        {
+            enemy.destination = (player.transform.position);
+            //transform.position += enemy.desiredVelocity * Time.deltaTime;
+        }
+
+        if (anim.GetBool("isFiring"))
+        {
+            enemy.destination = transform.position;
+            //gun.transform.LookAt(transform.InverseTransformDirection(Vector3.forward)); 
+        }
+
+        if (anim.GetBool("isWalking"))
+        {
+            if (enemy.desiredVelocity.magnitude < 0.1f && !enemy.pathPending)
+            {
+                target = potentialTargets[Random.Range(0, potentialTargets.Length)];
+                enemy.destination = (target.transform.position);
+            }
+            //transform.position += enemy.desiredVelocity * Time.deltaTime;
+        }
+
+        if (anim.GetBool("isIdle"))
+        {
+            enemy.destination = (transform.position);
+        }     
+    }
+
+    
+
+    
+
+    public enum StateEnum
+    {
+        RUN,
+        //SHOOT
+    }
+}
