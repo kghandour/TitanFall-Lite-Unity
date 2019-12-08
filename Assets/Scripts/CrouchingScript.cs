b1bf0c5a6f0e17eb3e72072832e2f5100c992a98
Amjad Mashaal <me@amjad.io>
2019-12-08 17:32:45 +0200

    Initial crouching code

---
+++ Assets/Scripts/CrouchingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchingScript : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetBool("isCrouching", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            anim.SetBool("isCrouching", false);
+        }
+    }
+}
