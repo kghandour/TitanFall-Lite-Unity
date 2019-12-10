5fcee0462c54a04d1be6a28f95ed629eb86061e1
omarelmoghazy <omarelmoghazy1@gmail.com>
2019-12-10 22:23:18 +0200

    Shield update

--- Assets/Scripts/sheild.cs
+++ Assets/Scripts/sheild.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class sheild : MonoBehaviour
 {
-    private int countBullets = 0;
-    void OnCollisionEnter(Collision collisionInfo)
-    {
-        if(collisionInfo.collider.tag == "Bullet")
-        {
-            countBullets = countBullets + 1;
-            Destroy(collisionInfo.collider.gameObject);
-        }
-    }
+    public int bulletsTotalDamage = 0;
+    //void OnCollisionEnter(Collision collisionInfo)
+    //{
+    //    if(collisionInfo.collider.tag == "Bullet")
+    //    {
+    //        countBullets = countBullets + 1;
+    //        Destroy(collisionInfo.collider.gameObject);
+    //    }
+    //}
     // Start is called before the first frame update
     void Start()
     {
