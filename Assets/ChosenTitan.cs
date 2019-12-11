8d1c22abf1fa76ab68b970f36f0907aa77b0d785
Karim ElGhandour <kghandour@live.com>
2019-12-12 00:28:52 +0200

    Chosen Titan

--- Assets/ChosenTitan.cs
+++ Assets/ChosenTitan.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChosenTitan : MonoBehaviour
 {
-    public static int selectedTitan=1;
+    public static int selectedTitan=0   ;
     // Start is called before the first frame update
     void Start()
     {
